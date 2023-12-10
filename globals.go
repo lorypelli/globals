c70499974ebdf8f10e7aafb27c24a45f541e9e7a
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-12-10 11:41:06 +0100

    changed comment to be single line

--- globals.go
+++ globals.go
@@ -5,9 +5,7 @@ import (
 	"strings"
 )
 
-/*Function to split files with newlines.
-It returns an array where each member is the newline content.
-Made this for AOC to help users*/
+//Function to split files with newlines. It returns an array where each member is the newline content. Made this for AOC to help users
 func SplitFile(fileName string) []string {
 	file, _ := os.Open(fileName)
 	stats, _ := os.Stat(fileName)
